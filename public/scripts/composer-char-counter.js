099be9a24d3907a8bddfe430342e6cf12d3bb1d3
Jubril Bello <jbello@uoguelph.ca>
2023-08-09 02:51:28 -0400

    Fix displaying styling issues

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
-  console.log("working");
+  // Initialize the tweet-text input event listener
   $("#tweet-text").on("input", function () {
     const textLength = $(this).val().length;
-    console.log("working 2");
+    const remainingChars = 140 - textLength; // Calculate remaining characters
     console.log(textLength);
-    const remainingChars = 140 - textLength;
     const counterElement = $("#char-counter");
-    counterElement.text(remainingChars);
+    counterElement.text(remainingChars); // Update the displayed counter
 
+    // Adjust the styling based on the remaining character count
     if (remainingChars < 0) {
       counterElement.addClass("over-limit");
     } else {
