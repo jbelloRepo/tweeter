0cbe2952fe14a0c8c411fe5bed821f2d693c4bb2
Jubril Bello <jbello@uoguelph.ca>
2023-08-09 00:55:18 -0400

    Implement XSS prevention

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,9 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Escaping function to prevent Cross-Site Scripting (XSS)
+const escape = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
